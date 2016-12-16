dd32e8435e59a896a9880a7eecc1e82c702b5e7d
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2016-12-16 13:36:10 +1300

    Rare direct commit correcting just one character in a comment with no other changes

--- service.rb
+++ service.rb
@@ -1,5 +1,5 @@
 # Top level service class that describes the component interfaces. This is
-# run by Config.ru. The class must be called "ServiceApplication" (unless
+# run by config.ru. The class must be called "ServiceApplication" (unless
 # "config.ru" has been modified).
 
 class ServiceApplication < Hoodoo::Services::Service
